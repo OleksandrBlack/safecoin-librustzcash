cece730d83a0983275f93d34a4b822c8d3d4000a
OleksandrBlack <aleksandr.chornuy@gmail.com>
2020-09-17 19:21:30 +0300

    up

--- zcash_client_backend/src/constants/mainnet.rs
+++ zcash_client_backend/src/constants/mainnet.rs
@@ -1,4 +1,4 @@
-/// The mainnet coin type for ZEC, as defined by [SLIP 44].
+/// The mainnet coin type for Safecoin, as defined by [SLIP 44].
 ///
 /// [SLIP 44]: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 pub const COIN_TYPE: u32 = 19165;
@@ -9,7 +9,7 @@ pub const COIN_TYPE: u32 = 19165;
 ///
 /// [`ExtendedSpendingKey`]: zcash_primitives::zip32::ExtendedSpendingKey
 /// [ZIP 32]: https://github.com/zcash/zips/blob/master/zip-0032.rst
-pub const HRP_SAPLING_EXTENDED_SPENDING_KEY: &str = "secret-extended-key-main";
+pub const HRP_SAPLING_EXTENDED_SPENDING_KEY: &str = "secret-extended-key-safe";
 
 /// The HRP for a Bech32-encoded mainnet [`ExtendedFullViewingKey`].
 ///
@@ -17,7 +17,7 @@ pub const HRP_SAPLING_EXTENDED_SPENDING_KEY: &str = "secret-extended-key-main";
 ///
 /// [`ExtendedFullViewingKey`]: zcash_primitives::zip32::ExtendedFullViewingKey
 /// [ZIP 32]: https://github.com/zcash/zips/blob/master/zip-0032.rst
-pub const HRP_SAPLING_EXTENDED_FULL_VIEWING_KEY: &str = "zxviews";
+pub const HRP_SAPLING_EXTENDED_FULL_VIEWING_KEY: &str = "sviews";
 
 /// The HRP for a Bech32-encoded mainnet [`PaymentAddress`].
 ///
